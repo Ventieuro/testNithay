6a927363d8f4afb3eb7c4fb8b1939de10b1f025f
Nithay <npgomezsilva@studenti.uninsubria.it>
2026-05-06 19:19:30 +0200

    CiaoMondofile

---
+++ CiaoMondo.java
@@ -0,0 +1,10 @@
+public class CiaoMondo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ciao Mondo!A");
+		System.out.println("1"+"2");
+		System.out.println(1+2);
+	}
+
+}
